457a42f91eed417d35ab0967e69f23c2d0a4a441
vma5197 <vma5197@psu.edu>
2025-10-18 08:46:07 -0400

    Fix: updated metro.config.js to use expo/metro-config and add asset plugin for fonts and SVGs

--- mobile/expo/metro.config.js
+++ mobile/expo/metro.config.js
@@ -1,10 +1,11 @@
-const { getDefaultConfig } = require('metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = (async () => {
-  const config = await getDefaultConfig();
-  const { assetExts, sourceExts } = config.resolver;
-  config.resolver.assetExts = assetExts.filter(ext => ext !== 'svg');
-  config.resolver.sourceExts = [...sourceExts, 'svg'];
-  config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
-  return config;
-})();
+const config = getDefaultConfig(__dirname);
+
+config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
+config.transformer.assetPlugins = ['expo-asset/tools/hashAssetFiles'];
+
+config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'svg');
+config.resolver.sourceExts.push('svg');
+
+module.exports = config;
